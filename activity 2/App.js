558b4a887da7283a58c5a561cc503f2a6505dfbf
romeroso <jmilromeroso@gmail.com>
2025-01-27 12:40:10 +0800

    MultiTextInput

---
+++ activity 2/App.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const TextInputExample = () => {
+  const [text, onChangeText] = React.useState('Name:');
+  const[number, onChangNumber] = React.useState('');
+  
+  return(
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <TextInput
+        style={style.input}
+        onChangeText={onChangeText}
+        value={text}
+        />
+
+        <TextInput
+          style={style.input}
+          onChangeText={onChangNumber}
+          Value={number}
+          placeholder="Age:"
+          keyboardType="numeric"
+          />
+
+           <TextInput
+          style={style.input}
+          onChangeText={onChangNumber}
+          Value={number}
+          placeholder="Address:"
+          keyboardType="numeric"
+          />
+          
+           <TextInput
+          style={style.input}
+          onChangeText={onChangNumber}
+          Value={number}
+          placeholder="School:"
+          keyboardType="numeric"
+          />
+
+           <TextInput
+          style={style.input}
+          onChangeText={onChangNumber}
+          Value={number}
+          placeholder="Course:"
+          keyboardType="numeric"
+          />
+
+           <TextInput
+          style={style.input}
+          onChangeText={onChangNumber}
+          Value={number}
+          placeholder="email:"
+          keyboardType="numeric"
+          />
+
+           <TextInput
+          style={style.input}
+          onChangeText={onChangNumber}
+          Value={number}
+          placeholder="Contact:"
+          keyboardType="tel"
+          />
+
+          <TextInput
+          multiline
+          numberOfLines={5}
+          editable
+          style={style.multi}
+          placeholder="About Me"
+          maxLength={255}
+        />
+
+        </SafeAreaView>
+      </SafeAreaProvider>
+  );
+};
+
+const style = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  multi: {
+    height: 100,
+    margin: 12,
+    bordercolor: '#000008B',
+    borderWidth: 2,
+    padding: 5,
+  },
+});
+
+
+export default TextInputExample;
+
+
